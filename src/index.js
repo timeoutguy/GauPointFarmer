0e53e6b1a25e8587c5b91d2123414d5ce34eebd0
MARCUS VINICIUS SOUZA SILVA <marcus.silva58@fatec.sp.gov.br>
2020-06-07 15:58:24 -0300

    🚀 Estrutura básica definida. Já faz login, só falta arrumar o SMS token

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+const readline = require('readline');
+const credentials = require('./credentials');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.setViewport({
+    width: 1280,
+    height: 800,
+  });
+
+  await page.goto('https://www.twitch.tv/gaules', { waitUntil: 'networkidle2' });
+  await page.click('div[data-a-target="tw-core-button-label-text"]');
+  await page.waitForSelector('#login-username');
+  await page.type('#login-username', credentials.username, { delay: 25 });
+  await page.type('#password-input', credentials.password, { delay: 25 });
+  await page.click('button[data-a-target="passport-login-button"]');
+  rl.question('Input your SMS token: ', (token) => {
+    page.type('input[autocomplete="one-time-code"]', token);
+    page.click('div[data-a-target="tw-core-button-label-text"]');
+  });
+})();
